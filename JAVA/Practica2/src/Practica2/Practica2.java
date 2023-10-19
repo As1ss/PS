a52fdd06ba4d4381172787429e626aa785e36974
As1ss <alexis.lopbri@educa.jcyl.es>
2023-10-19 17:11:58 +0200

    Actividad 2 realizada y completada

---
+++ JAVA/Practica2/src/Practica2/Practica2.java
@@ -0,0 +1,20 @@
+package Practica2;
+
+import java.util.Arrays;
+
+public class Practica2 {
+
+	public static void main(String[] args) {
+		
+		
+		if(args.length!=0) {
+			System.out.println(Arrays.toString(args));
+			
+		}
+		else {
+			System.out.println("Por favor introduczca algún valor");
+		}
+
+	}
+
+}
