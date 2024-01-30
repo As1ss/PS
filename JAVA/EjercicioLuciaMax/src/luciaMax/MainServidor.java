e74e9c4c0fd553351a36c86e4f041353eaa45aee
As1ss <alexis.lopbri@educa.jcyl.es>
2024-01-30 21:42:28 +0100

    .

---
+++ JAVA/EjercicioLuciaMax/src/luciaMax/MainServidor.java
@@ -0,0 +1,13 @@
+package luciaMax;
+
+
+public class MainServidor {
+
+	public static void main(String[] args) {
+		//Creamos el servidor en el puerto 1234
+		Servidor servidor = new Servidor(1234);
+		//Lo ponemos a escuchar
+		servidor.start();
+	}
+
+}
